e4f155dde2c9f8093f2c19355d51e3207015a26a
Gerardo Martín <gerardo@pop-os.localdomain>
2023-03-28 13:38:07 -0600

    Preguntas intra

--- Scripts-R/Matrices-proyeccion.R
+++ Scripts-R/Matrices-proyeccion.R
@@ -1,17 +1,16 @@
 ## Matrices de proyección poblacional
 s01 <- 0.5; s12 <- 0.5; s23 <- 0.5; s34 <- 0.5; s45 <- 0.5
-f3 <- 5; f4 <- 5; f5 <- 5
-
+f3 <- 0; f4 <- 5; f5 <- 5
 
 A <- matrix(c(0,   0, s01*f3, s01*f4, s01*f5,
               s12, 0, 0 ,     0,      0,
               0,  s23,0,      0,      0,
               0,  0,  s34,    0,      0,
-              0,  0,  0,      s45,    0), byrow = T, ncol = 5)
+              0,  0,  0,      s45,    0), byrow = T, ncol = 5, nrow = 5)
 
 N0 <- c(0, 0, 10, 10, 10)
 
-tiempo <- 25
+tiempo <- 50
 
 N <- matrix(0, ncol = tiempo, nrow = 5)
 N[, 1] <- N0
